f8b603f54797977b9610585718fa6a91628d77c4
CaXaP <sasha.bazdyrev2004@gmail.com>
2023-05-12 01:35:45 +0700

    playing around

--- app/src/main/java/com/example/codeinterpretator/MainActivity.kt
+++ app/src/main/java/com/example/codeinterpretator/MainActivity.kt
@@ -35,6 +35,83 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Preview(widthDp = 320, heightDp = 300)
+@Composable
+fun LazyColumnSample() {
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+    ) {
+        items(100) { position ->
+
+            println("Build item at position $position")
+
+            Row {
+                MyListItem(
+                    position = position,
+                    color = Color.Gray,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+    }
+
+    @Preview(widthDp = 320, heightDp = 50)
+    @Composable
+    fun MyListItem(
+        position: Int = 0,
+        color: Color = Color.Gray,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 0.9f)
+                .padding(4.dp)
+                .height(42.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .aspectRatio(1.0f, matchHeightConstraintsFirst = true)
+                    .clip(CircleShape)
+                    .background(color)
+            ) {
+                Text(
+                    text = position.toString(),
+                    style = MaterialTheme.typography.subtitle1,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(6.dp))
+
+            Column(
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(12.dp)
+                        .clip(shape = RoundedCornerShape(2.dp))
+                        .background(color)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(fraction = 0.6f)
+                        .height(8.dp)
+                        .clip(shape = RoundedCornerShape(2.dp))
+                        .background(color)
+                )
+            }
+        }
+    }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
